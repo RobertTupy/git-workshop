69f051e3200066545e4e0ae53a416b34ce9b0863
Robert Tupy <tupy@gpsdozor.cz>
2024-03-20 18:19:05 +0100

    Add example base

---
+++ helpers.js
@@ -0,0 +1,14 @@
+function getRandomDate() {
+    const end = new Date();
+    end.setFullYear(end.getFullYear() - 18);
+
+    const start = new Date();
+    start.setFullYear(start.getFullYear() - 48);
+
+    return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+}
+
+const example = [].map(name => ({
+    name: name,
+    birthday: getRandomDate()
+}))
